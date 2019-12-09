002ab2fa214ad2e861a9db0d1fee6662160d0249
ci <ci>
2019-12-09 18:14:18 +0000

    init

---
+++ implementors/core/marker/trait.Freeze.js
@@ -0,0 +1,9 @@
+(function() {var implementors = {};
+implementors["rustc_demangle"] = [{text:"impl&lt;'a&gt; Freeze for <a class=\"struct\" href=\"rustc_demangle/struct.Demangle.html\" title=\"struct rustc_demangle::Demangle\">Demangle</a>&lt;'a&gt;",synthetic:true,types:["rustc_demangle::Demangle"]},{text:"impl Freeze for <a class=\"struct\" href=\"rustc_demangle/struct.TryDemangleError.html\" title=\"struct rustc_demangle::TryDemangleError\">TryDemangleError</a>",synthetic:true,types:["rustc_demangle::TryDemangleError"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        })()
---
+++ source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["rustc_demangle"] = {"name":"","files":["legacy.rs","lib.rs","v0.rs"]};
+createSourceSidebar();
